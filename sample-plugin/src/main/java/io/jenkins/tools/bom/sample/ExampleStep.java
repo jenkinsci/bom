dd7d34924e25f30f8e5a64fc198a6db8e0bb3567
Jesse Glick <jglick@cloudbees.com>
2019-07-08 17:57:29 -0400

    It is also possible to manage the versions of test JARs.

--- sample-plugin/src/main/java/io/jenkins/tools/bom/sample/ExampleStep.java
+++ sample-plugin/src/main/java/io/jenkins/tools/bom/sample/ExampleStep.java
@@ -10,23 +10,29 @@ import org.jenkinsci.plugins.workflow.steps.StepDescriptor;
 import org.jenkinsci.plugins.workflow.steps.StepExecution;
 import org.jenkinsci.plugins.workflow.steps.SynchronousStepExecution;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
 
 public final class ExampleStep extends Step {
+
+    @DataBoundSetter public String x;
 
     @DataBoundConstructor public ExampleStep() {}
 
     @Override public StepExecution start(StepContext context) throws Exception {
-        return new Execution(context);
+        return new Execution(context, x);
     }
 
     private static final class Execution extends SynchronousStepExecution<Void> {
 
-        Execution(StepContext context) {
+        private final String x;
+
+        Execution(StepContext context, String x) {
             super(context);
+            this.x = x;
         }
         
         @Override protected Void run() throws Exception {
-            getContext().get(TaskListener.class).getLogger().println("Ran example step!");
+            getContext().get(TaskListener.class).getLogger().println("Ran on " + x + "!");
             return null;
         }
 
--- sample-plugin/src/test/java/io/jenkins/tools/bom/sample/ExampleStepTest.java
+++ sample-plugin/src/test/java/io/jenkins/tools/bom/sample/ExampleStepTest.java
@@ -3,6 +3,8 @@ package io.jenkins.tools.bom.sample;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.jenkinsci.plugins.workflow.steps.StepConfigTester;
+import static org.junit.Assert.*;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.Rule;
@@ -17,10 +19,14 @@ public class ExampleStepTest {
 
     @Test public void smokes() throws Exception {
         WorkflowJob p = r.createProject(WorkflowJob.class);
-        p.setDefinition(new CpsFlowDefinition("node {example()}; echo 'more stuff too'", true));
+        p.setDefinition(new CpsFlowDefinition("node {example(x: 'some value')}; echo 'more stuff too'", true));
         WorkflowRun b = r.buildAndAssertSuccess(p);
-        r.assertLogContains("Ran example step!", b);
+        r.assertLogContains("Ran on some value!", b);
         r.assertLogContains("more stuff too", b);
+        ExampleStep s = new ExampleStep();
+        s.x = "sample";
+        ExampleStep s2 = new StepConfigTester(r).configRoundTrip(s);
+        assertEquals("sample", s2.x);
     }
 
 }
