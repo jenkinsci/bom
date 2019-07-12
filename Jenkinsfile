4772c150740bbb091001797295c7ad8f40073598
Jesse Glick <jglick@cloudbees.com>
2019-07-12 09:35:56 -0400

    Using JDK & Maven tools, as well as the Azure mirror.

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,6 +1,11 @@
 node('docker') {
+    def settingsXml = "${pwd tmp: true}/settings-azure.xml"
+    def ok = infra.retrieveMavenSettingsFile(settingsXml)
+    assert ok
     checkout scm
-    sh 'bash ci.sh'
+    withEnv(["JAVA_HOME=${tool 'jdk8'}", 'PATH+JAVA=${JAVA_HOME}/bin', "PATH+MAVEN=${tool 'mvn'}/bin", "MAVEN_SETTINGS=$settingsXml"]) {
+        sh 'bash ci.sh'
+    }
     junit '**/target/surefire-reports/TEST-*.xml'
     warnError('some plugins could not be run in PCT') {
         sh 'fgrep -L "<status>INTERNAL_ERROR</status>" sample-plugin/target/pct-report.xml'
--- ci.sh
+++ ci.sh
@@ -2,9 +2,17 @@
 set -euxo pipefail
 cd $(dirname $0)
 
-# TODO add -ntp everywhere after INFRA-2129 / Maven 3.6.1
+# TODO use -ntp after INFRA-2129 / Maven 3.6.1
+MVN='mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn'
+if [ -v MAVEN_SETTINGS ]
+then
+    MVN="$MVN -s $MAVEN_SETTINGS"
+    PCT_S_ARG="-m2SettingsFile $MAVEN_SETTINGS"
+else
+    PCT_S_ARG=
+fi
 
-mvn -B -Dmaven.test.failure.ignore install
+$MVN -Dmaven.test.failure.ignore install
 
 cd sample-plugin/target
 cp -r jenkins-for-test megawar
@@ -15,14 +23,16 @@ cp -rv test-classes/test-dependencies/*.hpi megawar/WEB-INF/plugins
 
 version=0.1.0
 pct=$HOME/.m2/repository/org/jenkins-ci/tests/plugins-compat-tester-cli/$version/plugins-compat-tester-cli-$version.jar
-[ -f $pct ] || mvn -B dependency:get -Dartifact=org.jenkins-ci.tests:plugins-compat-tester-cli:$version:jar -Dtransitive=false
+[ -f $pct ] || $MVN dependency:get -Dartifact=org.jenkins-ci.tests:plugins-compat-tester-cli:$version:jar -Dtransitive=false
 
 # TODO perhaps stash the megawar and split the test runs across nodes in parallel? (use label `maven` for quick startup)
+# (means losing the ability to run Dockerized tests in PCT, e.g. in durable-task)
 java -jar $pct \
      -war $(pwd)/megawar.war \
      -workDirectory $(pwd)/pct-work \
      -reportFile $(pwd)/pct-report.xml \
      -mvn $(which mvn) \
+     $PCT_S_ARG
      -skipTestCache true
 
 # TODO currently failing tests: https://github.com/jenkinsci/workflow-cps-plugin/pull/302 https://github.com/jenkinsci/structs-plugin/pull/50
