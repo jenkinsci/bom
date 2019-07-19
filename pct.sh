732dde9d5dde5ae039519e5d82a06feafc4133ae
Jesse Glick <jglick@cloudbees.com>
2019-07-18 22:05:46 -0400

    https://github.com/jenkinsci/workflow-cps-plugin/pull/302 was released.

--- pct.sh
+++ pct.sh
@@ -29,8 +29,6 @@ java -jar pct.jar \
      -mavenProperties "$MAVEN_PROPERTIES" \
      -skipTestCache true
 
-# TODO https://github.com/jenkinsci/workflow-cps-plugin/pull/302
-rm -fv pct-work/workflow-cps/target/surefire-reports/TEST-org.jenkinsci.plugins.workflow.cps.SnippetizerTest.xml
 # TODO https://github.com/jenkinsci/structs-plugin/pull/50
 rm -fv pct-work/structs-plugin/plugin/target/surefire-reports/TEST-org.jenkinsci.plugins.structs.describable.DescribableModelTest.xml
 # TODO https://github.com/jenkinsci/jenkins/pull/4120 problems with workflow-cps → jquery-detached:
