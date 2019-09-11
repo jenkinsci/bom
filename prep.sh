aae149572fa469fd4cd26650600026e965835c6d
Basil Crow <me@basilcrow.com>
2019-09-11 08:43:08 -0700

    Pull in jenkinsci/plugin-compat-tester#181

--- prep.sh
+++ prep.sh
@@ -23,9 +23,10 @@ cp -rv test-classes/test-dependencies/*.hpi megawar/WEB-INF/plugins
 (cd megawar && jar c0Mf ../megawar.war *)
 
 # TODO find a way to encode this in some POM so that it can be managed by Dependabot
-version=0.2.0
-pct=$HOME/.m2/repository/org/jenkins-ci/tests/plugins-compat-tester-cli/$version/plugins-compat-tester-cli-$version.jar
-[ -f $pct ] || $MVN dependency:get -Dartifact=org.jenkins-ci.tests:plugins-compat-tester-cli:$version:jar -DremoteRepositories=https://repo.jenkins-ci.org/public/ -Dtransitive=false
+version=0.2.1
+timestamp=20190911.153925-1 # TODO https://github.com/jenkinsci/plugin-compat-tester/pull/181
+pct=$HOME/.m2/repository/org/jenkins-ci/tests/plugins-compat-tester-cli/${version}-SNAPSHOT/plugins-compat-tester-cli-${version}-${timestamp}.jar
+[ -f $pct ] || $MVN dependency:get -Dartifact=org.jenkins-ci.tests:plugins-compat-tester-cli:${version}-${timestamp}:jar -DremoteRepositories=https://repo.jenkins-ci.org/public/ -Dtransitive=false
 
 cp $pct pct.jar
 cd megawar/WEB-INF/plugins
