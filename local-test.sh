5789d1874eedc771d0cd9c841c499f6663afa4b5
Basil Crow <me@basilcrow.com>
2022-03-21 17:15:22 -0700

    Make shell scripts `shfmt(1)` and `shellcheck(1)` clean (#950)

--- local-test.sh
+++ local-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
-cd "$(dirname $0)"
+cd "$(dirname "$0")"
 
 # expects: $PLUGINS, optionally $TEST, $LINE
 
@@ -8,9 +8,8 @@ LATEST_LINE=weekly
 : "${LINE:=$LATEST_LINE}"
 
 export SAMPLE_PLUGIN_OPTS=-Dtest=InjectedTest
-if [ $LINE \!= $LATEST_LINE ]
-then
-    export SAMPLE_PLUGIN_OPTS="$SAMPLE_PLUGIN_OPTS -P$LINE"
+if [ $LINE \!= $LATEST_LINE ]; then
+	export SAMPLE_PLUGIN_OPTS="${SAMPLE_PLUGIN_OPTS} -P${LINE}"
 fi
 LINEZ=$LINE bash prep.sh
 
@@ -19,30 +18,28 @@ mkdir target/local-test
 cp -v target/pct.jar pct.sh excludes.txt target/local-test
 cp -v target/megawar-$LINE.war target/local-test/megawar.war
 
-if [ -v TEST ]
-then
-    EXTRA_MAVEN_PROPERTIES="test=$TEST"
+if [ -v TEST ]; then
+	EXTRA_MAVEN_PROPERTIES="test=${TEST}"
 else
-    EXTRA_MAVEN_PROPERTIES=
+	EXTRA_MAVEN_PROPERTIES=
 fi
 
-if [ -v DOCKERIZED ]
-then
-    docker volume inspect m2repo || docker volume create m2repo
-    docker run \
-           -v ~/.m2:/var/maven/.m2 \
-           --rm \
-           --name bom-pct \
-           -v "$(pwd)"/target/local-test:/pct \
-           -e MAVEN_OPTS=-Duser.home=/var/maven \
-           -e MAVEN_CONFIG=/var/maven/.m2 \
-           -e PLUGINS="$PLUGINS" \
-           -e LINE=$LINE \
-           -e EXTRA_MAVEN_PROPERTIES=$EXTRA_MAVEN_PROPERTIES \
-           --entrypoint bash \
-           jenkins/jnlp-agent-maven \
-           -c "trap 'chown -R $(id -u):$(id -g) /pct /var/maven/.m2/repository' EXIT; bash /pct/pct.sh"
+if [ -v DOCKERIZED ]; then
+	docker volume inspect m2repo || docker volume create m2repo
+	docker run \
+		-v ~/.m2:/var/maven/.m2 \
+		--rm \
+		--name bom-pct \
+		-v "$(pwd)/target/local-test:/pct" \
+		-e MAVEN_OPTS=-Duser.home=/var/maven \
+		-e MAVEN_CONFIG=/var/maven/.m2 \
+		-e "PLUGINS=${PLUGINS}" \
+		-e "LINE=${LINE}" \
+		-e "EXTRA_MAVEN_PROPERTIES=${EXTRA_MAVEN_PROPERTIES}" \
+		--entrypoint bash \
+		jenkins/jnlp-agent-maven \
+		-c "trap 'chown -R $(id -u):$(id -g) /pct /var/maven/.m2/repository' EXIT; bash /pct/pct.sh"
 else
-    export EXTRA_MAVEN_PROPERTIES
-    LINE=$LINE bash target/local-test/pct.sh
+	export EXTRA_MAVEN_PROPERTIES
+	LINE=$LINE bash target/local-test/pct.sh
 fi
