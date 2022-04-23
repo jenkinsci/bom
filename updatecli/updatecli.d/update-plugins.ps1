6a405f9b63552bd35c82976da3069323d7b0390e
Joseph Petersen <josephp90@gmail.com>
2022-04-23 22:06:09 +0200

    add updatecli for plugins (#1063)
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>

---
+++ updatecli/updatecli.d/update-plugins.ps1
@@ -0,0 +1,41 @@
+param(
+  [string] $JenkinsVersion,
+  [string] $PluginManagerJar = './plugin-manager.jar'
+)
+
+$JenkinsVersionX = $JenkinsVersion -replace '\d+$', 'x'
+
+if (Get-Command 'java' -ErrorAction SilentlyContinue) {
+  $java = 'java'
+} elseif ($IsLinux) {
+  $java = '/usr/bin/java'
+} else {
+  $java = 'java'
+}
+
+$pomPath = "bom-${JenkinsVersionX}/pom.xml"
+$pom = New-Object System.Xml.XmlDocument
+$pom.PreserveWhitespace = $true
+$pom.Load($pomPath)
+
+# Select Dependencies and Skip BOM
+$dependencies = $pom.Project.DependencyManagement.Dependencies.Dependency | Select-Object -Skip 1
+
+foreach ($dependency in $dependencies) {
+  $artifact = $dependency.artifactId
+  $oldVersion = $dependency.version
+  $plugin = "${artifact}:${oldVersion}"
+  [string] $output = & $java -jar "$PluginManagerJar" --no-download --available-updates --jenkins-version "$JenkinsVersion" --plugins $plugin
+  if ($null -ne $output -and $output -inotlike '*No available updates*') {
+    # Example output:
+    # Available updates:
+    # credentials (2.6.1) has an available update: 2.6.1.1
+
+    # Grab the version number at the end of the line
+    $newVersion = ($output | Select-String -Pattern 'update: (.+)').matches.Groups[1].Value
+    Write-Output "Changed $artifact from $oldVersion to $newVersion"
+    $dependency.version = $newVersion
+  }
+}
+
+$pom.Save($pomPath)
---
+++ updatecli/updatecli.d/update-plugins.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eux -o pipefail
+
+jenkinsVersion="$1"
+jar="${PLUGIN_MANAGER_JAR_PATH:-"./plugin-manager.jar"}"
+
+# check if jar does not exist
+if [ ! -f "$jar" ]; then
+  curl -sSL https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/2.12.3/jenkins-plugin-manager-2.12.3.jar -o "$jar"
+fi
+
+pwsh -NoProfile -File "./updatecli/updatecli.d/update-plugins.ps1" -JenkinsVersion "${jenkinsVersion}" -PluginManagerJar "${jar}"
