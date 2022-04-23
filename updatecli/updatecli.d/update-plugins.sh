bff99fa3336f7f0333632cd6fa90831ea039b0ed
Joseph Petersen <josephp90@gmail.com>
2022-04-23 14:16:02 +0200

    download plugin manager to updatecli workspace

--- updatecli/updatecli.d/update-plugins.sh
+++ updatecli/updatecli.d/update-plugins.sh
@@ -4,4 +4,10 @@ set -eux -o pipefail
 
 jenkinsVersion="$1"
 jar="${PLUGIN_MANAGER_JAR_PATH:-"./plugin-manager.jar"}"
+
+# check if jar does not exist
+if [ ! -f "$jar" ]; then
+  curl -sSL https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/2.12.3/jenkins-plugin-manager-2.12.3.jar -o "$jar"
+fi
+
 pwsh -NoProfile -File "./updatecli/updatecli.d/update-plugins.ps1" -JenkinsVersion "${jenkinsVersion}" -PluginManagerJar "${jar}"
