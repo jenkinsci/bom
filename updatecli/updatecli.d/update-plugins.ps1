f1bc4e78e0205521ee1d1ddb012a3e6f0065e2ca
Joseph Petersen <josephp90@gmail.com>
2022-07-19 21:05:46 +0200

    simplify if condition

--- updatecli/updatecli.d/update-plugins.ps1
+++ updatecli/updatecli.d/update-plugins.ps1
@@ -34,7 +34,7 @@ foreach ($dependency in $dependencies) {
   $oldVersion = $dependency.version
   $plugin = "${artifact}:${oldVersion}"
   [string] $output = & $java -jar "$pluginManagerJar" --no-download --available-updates --jenkins-version "$JenkinsVersion" --plugins $plugin
-  if ($null -ne $output -and $output -inotlike '*No available updates*') {
+  if ($output -ilike '*has an available update*') {
     # Example output:
     # Available updates:
     # credentials (2.6.1) has an available update: 2.6.1.1
