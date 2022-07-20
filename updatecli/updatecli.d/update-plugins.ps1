23f25624e722935226d5d0972bca4def98dc9b57
Joseph Petersen <josephp90@gmail.com>
2022-07-20 21:42:41 +0200

    updatecli validate output (#1290)

--- updatecli/updatecli.d/update-plugins.ps1
+++ updatecli/updatecli.d/update-plugins.ps1
@@ -10,15 +10,7 @@ $pluginManagerVersion = $ENV:PLUGIN_MANAGER_VERSION ?? '2.12.8'
 
 # check if jar does not exist, download it - useful for testing
 if ([System.IO.File]::Exists($pluginManagerJar) -eq $false) {
-  curl -sSL "https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/$pluginManagerVersion/jenkins-plugin-manager-$pluginManagerVersion.jar" -o "$pluginManagerJar"
-}
-
-if (Get-Command 'java' -ErrorAction SilentlyContinue) {
-  $java = 'java'
-} elseif ($IsLinux) {
-  $java = '/usr/bin/java'
-} else {
-  $java = 'java'
+  & curl -sSL "https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/$pluginManagerVersion/jenkins-plugin-manager-$pluginManagerVersion.jar" -o "$pluginManagerJar"
 }
 
 $pomPath = "bom-${JenkinsVersionX}/pom.xml"
@@ -29,12 +21,13 @@ $pom.Load($pomPath)
 # Select Dependencies and Skip BOM
 $dependencies = $pom.Project.DependencyManagement.Dependencies.Dependency | Select-Object -Skip 1
 
+$changed = $false
 foreach ($dependency in $dependencies) {
   $artifact = $dependency.artifactId
   $oldVersion = $dependency.version
   $plugin = "${artifact}:${oldVersion}"
-  [string] $output = & $java -jar "$pluginManagerJar" --no-download --available-updates --output txt --jenkins-version "$JenkinsVersion" --plugins $plugin
-  if ($output -ne $plugin) {
+  [string] $output = & java -jar "$pluginManagerJar" --no-download --available-updates --output txt --jenkins-version "$JenkinsVersion" --plugins "$plugin"
+  if ($null -ne $output -and $output -ne $plugin) {
     # Example output:
     # credentials:2.6.1.1
 
@@ -42,10 +35,13 @@ foreach ($dependency in $dependencies) {
     $newVersion = $output.Split(':')[-1]
     Write-Output "Changed $artifact from $oldVersion to $newVersion"
     $dependency.version = $newVersion
+    $changed = $true
   }
 }
 
-$utf8WithoutBom = New-Object System.Text.UTF8Encoding($false)
-$streamWriter = New-Object System.IO.StreamWriter($pomPath, $false, $utf8WithoutBom)
-$pom.Save($streamWriter)
-$streamWriter.Close()
+if ($changed) {
+  $utf8WithoutBom = New-Object System.Text.UTF8Encoding($false)
+  $streamWriter = New-Object System.IO.StreamWriter($pomPath, $false, $utf8WithoutBom)
+  $pom.Save($streamWriter)
+  $streamWriter.Close()
+}
