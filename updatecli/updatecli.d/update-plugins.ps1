d388ae99f137e9ce377bd6915c0b1df240f0b98e
Joseph Petersen <josephp90@gmail.com>
2022-07-20 10:42:44 +0200

    use unreleased updatecli

--- updatecli/updatecli.d/update-plugins.ps1
+++ updatecli/updatecli.d/update-plugins.ps1
@@ -1,25 +1,16 @@
 param(
   [Parameter(Position=0)]
-  [string] $pluginManagerJar = './plugin-manager.jar',
-  [Parameter(Position=1)]
   [string] $JenkinsVersion
 )
 
 $JenkinsVersionX = $JenkinsVersion -replace '\d+$', 'x'
 
+$pluginManagerJar = $ENV:PLUGIN_MANAGER_JAR_PATH ?? './plugin-manager.jar'
 $pluginManagerVersion = $ENV:PLUGIN_MANAGER_VERSION ?? '2.12.8'
 
 # check if jar does not exist, download it - useful for testing
 if ([System.IO.File]::Exists($pluginManagerJar) -eq $false) {
-  curl -sSL "https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/$pluginManagerVersion/jenkins-plugin-manager-$pluginManagerVersion.jar" -o "$pluginManagerJar"
-}
-
-if (Get-Command 'java' -ErrorAction SilentlyContinue) {
-  $java = 'java'
-} elseif ($IsLinux) {
-  $java = '/usr/bin/java'
-} else {
-  $java = 'java'
+  & curl -sSL "https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/$pluginManagerVersion/jenkins-plugin-manager-$pluginManagerVersion.jar" -o "$pluginManagerJar"
 }
 
 $pomPath = "bom-${JenkinsVersionX}/pom.xml"
@@ -35,7 +26,7 @@ foreach ($dependency in $dependencies) {
   $artifact = $dependency.artifactId
   $oldVersion = $dependency.version
   $plugin = "${artifact}:${oldVersion}"
-  [string] $output = & $java -jar "$pluginManagerJar" --no-download --available-updates --output txt --jenkins-version "$JenkinsVersion" --plugins "$plugin"
+  [string] $output = & java -jar "$pluginManagerJar" --no-download --available-updates --output txt --jenkins-version "$JenkinsVersion" --plugins "$plugin"
   if ($null -ne $output -and $output -ne $plugin) {
     # Example output:
     # credentials:2.6.1.1
