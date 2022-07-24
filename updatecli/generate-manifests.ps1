c72b5475f27201c2e5a1069c527235de7928b719
Joseph Petersen <josephp90@gmail.com>
2022-07-24 13:35:03 +0200

    generate separete updatecli pipeline for each

--- updatecli/generate-manifests.ps1
+++ updatecli/generate-manifests.ps1
@@ -13,106 +13,13 @@ function EnsureModuleInstalled ($name) {
 EnsureModuleInstalled "powershell-yaml"
 
 $manifestDirectory = "$root/updatecli/updatecli.d"
-$updateJenkinsManifestPath = "$manifestDirectory/update-jenkins.yaml"
-$updatePluginsManifestPath = "$manifestDirectory/update-plugins.yaml"
 
 if ([System.IO.Directory]::Exists($manifestDirectory) -eq $false) {
   New-Item -Force -ItemType Directory -Path $manifestDirectory | Out-Null
 }
 
-# For testing locally, you can set $GITHUB_WORKSPACE to the root of the repository ie. $PWD
-$jenkinsUpdateScript = 'pwsh -NoProfile -File {{ requiredEnv "GITHUB_WORKSPACE" }}/updatecli/update-jenkins.ps1'
-
-$updatePluginsManifest = [ordered]@{
-  scms         = [ordered]@{
-    github = [ordered]@{
-      kind = "github"
-      spec = [ordered]@{
-        user       = '{{ .github.user }}'
-        email      = '{{ .github.email }}'
-        owner      = '{{ .github.owner }}'
-        repository = '{{ .github.repository }}'
-        branch     = '{{ .github.branch }}'
-        username   = '{{ .github.username }}'
-        token      = '{{ requiredEnv .github.token }}'
-      }
-    }
-  }
-  sources      = [ordered]@{}
-  conditions   = [ordered]@{}
-  targets      = [ordered]@{}
-  pullrequests = [ordered]@{}
-}
-
-$updateJenkinsManifest = [ordered]@{
-  scms         = [ordered]@{
-    github = [ordered]@{
-      kind = "github"
-      spec = [ordered]@{
-        user       = '{{ .github.user }}'
-        email      = '{{ .github.email }}'
-        owner      = '{{ .github.owner }}'
-        repository = '{{ .github.repository }}'
-        branch     = '{{ .github.branch }}'
-        username   = '{{ .github.username }}'
-        token      = '{{ requiredEnv .github.token }}'
-      }
-    }
-  }
-  sources      = [ordered]@{}
-  conditions   = [ordered]@{}
-  targets      = [ordered]@{}
-  pullrequests = [ordered]@{}
-}
-
-$updateJenkinsManifest.sources["jenkinsWeekly"] = [ordered]@{
-  name = "Get Last jenkins Weekly Version"
-  kind = "jenkins"
-  spec = [ordered]@{
-    release = "weekly"
-    github  = [ordered]@{
-      token    = '{{ requiredEnv .github.token }}'
-      username = '{{ .github.username }}'
-    }
-  }
-}
-$updateJenkinsManifest.conditions["jenkinsWeekly"] = [ordered]@{
-  name     = "Test if Jenkins weekly published"
-  kind     = "maven"
-  sourceid = "jenkinsWeekly"
-  spec     = [ordered]@{
-    url        = "repo.jenkins-ci.org"
-    repository = "releases"
-    groupId    = "org.jenkins-ci.main"
-    artifactId = "jenkins-war"
-  }
-}
-$updateJenkinsManifest.targets["jenkinsWeekly"] = [ordered]@{
-  name     = 'Update Jenkins weekly version'
-  sourceid = "jenkinsWeekly"
-  scmid    = "github"
-  kind     = "shell"
-  spec     = [ordered]@{
-    command      = $($jenkinsUpdateScript)
-    environments = @([ordered]@{
-        name = "GITHUB_WORKSPACE"
-      })
-  }
-}
-$updateJenkinsManifest.pullrequests["jenkinsWeekly"] = [ordered]@{
-  title   = 'Bump jenkins.version to {{ source "jenkinsWeekly" }} for bom-weekly'
-  kind    = "github"
-  scmid   = "github"
-  targets = @("jenkinsWeekly")
-  spec    = [ordered]@{
-    labels      = @("dependencies")
-    automerge   = $true
-    mergemethod = "squash"
-  }
-}
-
 # bills would be sorted by name due to our naming convention of "bom-x.y.z"
-$bills = Get-ChildItem "$root/bom-*" -Directory | Select-Object -ExpandProperty Name | Where-Object { $_ -ne "bom-weekly" }
+$bills = Get-ChildItem "$root/bom-*" -Directory | Select-Object -ExpandProperty Name
 
 $pomPath = "sample-plugin/pom.xml"
 $pom = New-Object System.Xml.XmlDocument
@@ -125,35 +32,74 @@ $pom.project.profiles.profile | ForEach-Object {
   $jenkinsVersions[$_.id] = $_.properties."jenkins.version"
 }
 
-$last = $bills[-1]
+$last = $bills | Where-Object { $_ -ne "bom-weekly" } | Select-Object -Last 1
 foreach ($bom in $bills) {
-  $version = $bom -replace "bom-", ""
-  $versionWithoutX = $version -replace ".x", ""
-  if ($last -eq $bom) {
-    $versionPattern = "jenkins-$versionWithoutX.1$"
-  } else {
-    $versionPattern = "jenkins-$versionWithoutX.\d+$"
-  }
-  $updateJenkinsManifest.sources["jenkins$version"] = [ordered]@{
-    name         = "Get last Jenkins stable version"
-    kind         = "githubrelease"
-    spec         = [ordered]@{
-      owner         = "jenkinsci"
-      repository    = "jenkins"
-      token         = '{{ requiredEnv .github.token }}'
-      versionfilter = [ordered]@{
-        kind    = "regex"
-        pattern = "$($versionPattern)"
+  $updateJenkinsManifest = [ordered]@{
+    scms         = [ordered]@{
+      github = [ordered]@{
+        kind = "github"
+        spec = [ordered]@{
+          user       = '{{ .github.user }}'
+          email      = '{{ .github.email }}'
+          owner      = '{{ .github.owner }}'
+          repository = '{{ .github.repository }}'
+          branch     = '{{ .github.branch }}'
+          username   = '{{ .github.username }}'
+          token      = '{{ requiredEnv .github.token }}'
+        }
       }
     }
-    transformers = @(@{
-        trimprefix = "jenkins-"
-      })
+    sources      = [ordered]@{}
+    conditions   = [ordered]@{}
+    targets      = [ordered]@{}
+    pullrequests = [ordered]@{}
   }
-  $updateJenkinsManifest.conditions["jenkins$version"] = [ordered]@{
+
+  $version = $bom -replace "bom-", ""
+  $versionWithoutX = $version -replace ".x", ""
+
+  $updateJenkinsManifestPath = "$manifestDirectory/update-jenkins-$version.yaml"
+
+  if ($bom -eq "bom-weekly") {
+    $updateJenkinsManifest.sources["jenkins"] = [ordered]@{
+      name = "Get Last jenkins Weekly Version"
+      kind = "jenkins"
+      spec = [ordered]@{
+        release = "weekly"
+        github  = [ordered]@{
+          token    = '{{ requiredEnv .github.token }}'
+          username = '{{ .github.username }}'
+        }
+      }
+    }
+  } else {
+    if ($last -eq $bom) {
+      $versionPattern = "jenkins-$versionWithoutX.1$"
+    } else {
+      $versionPattern = "jenkins-$versionWithoutX.\d+$"
+    }
+    $updateJenkinsManifest.sources["jenkins"] = [ordered]@{
+      name         = "Get last Jenkins stable version"
+      kind         = "githubrelease"
+      spec         = [ordered]@{
+        owner         = "jenkinsci"
+        repository    = "jenkins"
+        token         = '{{ requiredEnv .github.token }}'
+        versionfilter = [ordered]@{
+          kind    = "regex"
+          pattern = "$($versionPattern)"
+        }
+      }
+      transformers = @(@{
+          trimprefix = "jenkins-"
+        })
+    }
+  }
+
+  $updateJenkinsManifest.conditions["jenkins"] = [ordered]@{
     name     = "Test if Jenkins stable published"
     kind     = "maven"
-    sourceid = "jenkins$version"
+    sourceid = "jenkins"
     spec     = [ordered]@{
       url        = "repo.jenkins-ci.org"
       repository = "releases"
@@ -161,29 +107,28 @@ foreach ($bom in $bills) {
       artifactId = "jenkins-war"
     }
   }
-  $updateJenkinsManifest.targets["jenkins$version"] = [ordered]@{
-    name     = "Update Jenkins $version version"
-    sourceid = "jenkins$version"
+  $updateJenkinsManifest.targets["jenkins"] = [ordered]@{
+    name     = "Update Jenkins version"
+    sourceid = "jenkins"
     scmid    = "github"
     kind     = "shell"
     spec     = [ordered]@{
-      command      = $($jenkinsUpdateScript)
-      environments = @(
-        @{ name = "GITHUB_WORKSPACE" }
-      )
+      command      = 'pwsh -NoProfile -File {{ requiredEnv "GITHUB_WORKSPACE" }}/updatecli/update-jenkins.ps1'
     }
   }
-  $updateJenkinsManifest.pullrequests["jenkins$version"] = [ordered]@{
-    title   = "Bump jenkins.version to {{ source `"jenkins$version`" }} for bom-$version"
+  $updateJenkinsManifest.pullrequests["jenkins"] = [ordered]@{
+    title   = "Bump jenkins.version from $($jenkinsVersions[$version]) to {{ source `"jenkins`" }} for bom-$version"
     kind    = "github"
     scmid   = "github"
-    targets = @("jenkins$version")
+    targets = @("jenkins")
     spec    = [ordered]@{
       labels      = @("dependencies")
       automerge   = $true
       mergemethod = "squash"
     }
   }
+
+  ConvertTo-Yaml -Data $updateJenkinsManifest -OutFile $updateJenkinsManifestPath -Force
 
   $pomPath = "$bom/pom.xml"
   $pom = New-Object System.Xml.XmlDocument
@@ -198,12 +143,35 @@ foreach ($bom in $bills) {
     $groupId = $dependency.groupId
     $version = $dependency.version
 
-    if ($null -ne $updatePluginsManifest.sources["plugin${jenkinsVersion}${artifact}"]) {
+    $updatePluginsManifestPath = "$manifestDirectory/update-plugin-$jenkinsVersion-$artifact-$version.yaml"
+
+    if ([System.IO.File]::Exists($updatePluginsManifestPath)) {
       continue
     }
 
-    $updatePluginsManifest.sources["plugin${jenkinsVersion}${artifact}"] = [ordered]@{
-      name         = "Get last $artifact version for $version"
+    $updatePluginsManifest = [ordered]@{
+      scms         = [ordered]@{
+        github = [ordered]@{
+          kind = "github"
+          spec = [ordered]@{
+            user       = '{{ .github.user }}'
+            email      = '{{ .github.email }}'
+            owner      = '{{ .github.owner }}'
+            repository = '{{ .github.repository }}'
+            branch     = '{{ .github.branch }}'
+            username   = '{{ .github.username }}'
+            token      = '{{ requiredEnv .github.token }}'
+          }
+        }
+      }
+      sources      = [ordered]@{}
+      conditions   = [ordered]@{}
+      targets      = [ordered]@{}
+      pullrequests = [ordered]@{}
+    }
+
+    $updatePluginsManifest.sources["plugin"] = [ordered]@{
+      name         = "Get last $artifact version"
       kind         = "shell"
       spec         = @{
         command = "java -jar {{ requiredEnv `"PLUGIN_MANAGER_JAR_PATH`" }} --no-download --available-updates --output txt --jenkins-version $($jenkinsVersions[$jenkinsVersion]) --plugins ${artifact}:${version}"
@@ -217,10 +185,10 @@ foreach ($bom in $bills) {
         }
       )
     }
-    $updatePluginsManifest.conditions["plugin${jenkinsVersion}${artifact}"] = [ordered]@{
+    $updatePluginsManifest.conditions["plugin"] = [ordered]@{
       name     = "Test if $artifact is published"
       kind     = "maven"
-      sourceid = "plugin${jenkinsVersion}${artifact}"
+      sourceid = "plugin"
       spec     = [ordered]@{
         url        = "repo.jenkins-ci.org"
         repository = "releases"
@@ -228,28 +196,27 @@ foreach ($bom in $bills) {
         artifactId = "$($artifact)"
       }
     }
-    $updatePluginsManifest.targets["plugin${jenkinsVersion}${artifact}"] = [ordered]@{
+    $updatePluginsManifest.targets["plugin"] = [ordered]@{
       name     = "Update $artifact"
-      sourceid = "plugin${jenkinsVersion}${artifact}"
+      sourceid = "plugin"
       scmid    = "github"
       kind     = "shell"
       spec     = @{
         command = "pwsh -NoProfile -File {{ requiredEnv `"GITHUB_WORKSPACE`" }}/updatecli/update-plugin.ps1 $pomPath $artifact"
       }
     }
-    $updatePluginsManifest.pullrequests["plugin${jenkinsVersion}${artifact}"] = [ordered]@{
-      title   = "Bump $artifact to {{ source `"plugin${jenkinsVersion}${artifact}`" }} for bom-$version"
+    $updatePluginsManifest.pullrequests["plugin"] = [ordered]@{
+      title   = "Bump $artifact from $version to {{ source `"plugin`" }} in /bom-$jenkinsVersion"
       kind    = "github"
       scmid   = "github"
-      targets = @("plugin${jenkinsVersion}${artifact}")
+      targets = @("plugin")
       spec    = [ordered]@{
         labels      = @("dependencies")
         automerge   = $true
         mergemethod = "squash"
       }
     }
+
+    ConvertTo-Yaml -Data $updatePluginsManifest -OutFile $updatePluginsManifestPath -Force
   }
 }
-
-ConvertTo-Yaml -Data $updateJenkinsManifest -OutFile $updateJenkinsManifestPath -Force
-ConvertTo-Yaml -Data $updatePluginsManifest -OutFile $updatePluginsManifestPath -Force
