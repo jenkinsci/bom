6fef10d5045b5b3040ae1565a8c7addf4cef4865
Joseph Petersen <josephp90@gmail.com>
2022-07-24 11:03:44 +0200

    Remember to output change

--- updatecli/update-plugin.ps1
+++ updatecli/update-plugin.ps1
@@ -14,7 +14,7 @@ $pom.Load($PomPath)
 # Select Dependencies and Skip BOM
 $dependencies = $pom.Project.DependencyManagement.Dependencies.Dependency | Select-Object -Skip 1
 
-$plugin = $dependencies | Where-Object { $_.artifactid -eq $Artifact }
+$plugin = @($dependencies | Where-Object { $_.artifactid -eq $Artifact })
 
 if ($plugin.Count -eq 0) {
   Write-Host "Plugin not found"
@@ -28,6 +28,7 @@ if ($plugin[0].version -eq $NewVersion) {
 $plugin | ForEach-Object {
   $_.version = $NewVersion
 }
+Write-Output $NewVersion
 
 $utf8WithoutBom = New-Object System.Text.UTF8Encoding($false)
 $streamWriter = New-Object System.IO.StreamWriter($PomPath, $false, $utf8WithoutBom)
