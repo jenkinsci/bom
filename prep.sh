1593facefcce82127baaaf616576fd93c94b36d2
Devin Nusbaum <d.w.nusbaum@gmail.com>
2022-11-03 15:01:46 -0400

    Test changes related to converting workflow-cps into a multi-module project

--- prep.sh
+++ prep.sh
@@ -41,7 +41,7 @@ for LINE in $LINEZ; do
 done
 
 # TODO find a way to encode this in some POM so that it can be managed by Dependabot
-version=1190.v519c09131444
+version=1198.v72d48d41403a # https://github.com/jenkinsci/plugin-compat-tester/pull/383
 pct=$HOME/.m2/repository/org/jenkins-ci/tests/plugins-compat-tester-cli/${version}/plugins-compat-tester-cli-${version}.jar
 [ -f "${pct}" ] || $MVN dependency:get -Dartifact=org.jenkins-ci.tests:plugins-compat-tester-cli:${version}:jar -DremoteRepositories=https://repo.jenkins-ci.org/public/,https://repo.jenkins-ci.org/incrementals/ -Dtransitive=false
 cp "${pct}" target/pct.jar
