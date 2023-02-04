1a60ebc0b9ea889b4c2e70012998bb10a36deb51
Basil Crow <me@basilcrow.com>
2023-02-04 15:17:57 -0800

    Testing jenkinsci/plugin-compat-tester#435

--- prep.sh
+++ prep.sh
@@ -41,7 +41,8 @@ for LINE in $LINEZ; do
 done
 
 # Tracked by ./updatecli/updatecli.d/plugin-compat-tester.yml
-version=1226.v19b_a_68ffa_80e
+# TODO https://github.com/jenkinsci/plugin-compat-tester/pull/435
+version=1228.v0ee2f6b_d0177
 pct=$HOME/.m2/repository/org/jenkins-ci/tests/plugins-compat-tester-cli/${version}/plugins-compat-tester-cli-${version}.jar
 [ -f "${pct}" ] || $MVN dependency:get -Dartifact=org.jenkins-ci.tests:plugins-compat-tester-cli:${version}:jar -DremoteRepositories=https://repo.jenkins-ci.org/public/,https://repo.jenkins-ci.org/incrementals/ -Dtransitive=false
 cp "${pct}" target/pct.jar
