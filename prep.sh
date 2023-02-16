7f2336bfc41bc2d60080bba8441050968183943f
Hervé Le Meur <hlemeur@cloudbees.com>
2023-02-16 17:53:09 +0100

    fix remote repositories defaults

--- prep.sh
+++ prep.sh
@@ -44,7 +44,7 @@ done
 # TODO https://github.com/jenkinsci/plugin-compat-tester/pull/462
 version=1249.v93361f4ff6dd
 pct=$HOME/.m2/repository/org/jenkins-ci/tests/plugins-compat-tester-cli/${version}/plugins-compat-tester-cli-${version}.jar
-[ -f "${pct}" ] || $MVN dependency:get -Dartifact=org.jenkins-ci.tests:plugins-compat-tester-cli:${version}:jar -DremoteRepositories=https://repo.jenkins-ci.org/public/,https://repo.jenkins-ci.org/incrementals/ -Dtransitive=false
+[ -f "${pct}" ] || $MVN dependency:get -Dartifact=org.jenkins-ci.tests:plugins-compat-tester-cli:${version}:jar -DremoteRepositories=repo.jenkins-ci.org::default::https://repo.jenkins-ci.org/public/,incrementals::default::https://repo.jenkins-ci.org/incrementals/ -Dtransitive=false
 cp "${pct}" target/pct.jar
 
 # produces: target/{megawar-*.war,pct.jar,plugins.txt,lines.txt}
