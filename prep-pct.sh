470845c483d2f75a4cd86257beaa4c4a878ef106
Basil Crow <me@basilcrow.com>
2023-04-13 08:15:01 -0700

    Testing jenkinsci/plugin-compat-tester#510

--- prep-pct.sh
+++ prep-pct.sh
@@ -3,7 +3,8 @@ set -euxo pipefail
 cd "$(dirname "${0}")"
 
 # Tracked by ./updatecli/updatecli.d/plugin-compat-tester.yml
-pct_version=1307.veb_32a_fef160e
+# TODO https://github.com/jenkinsci/plugin-compat-tester/pull/510
+pct_version=1397.v505510ca_47d2
 pct="$(mvn -Dexpression=settings.localRepository -q -DforceStdout help:evaluate)/org/jenkins-ci/tests/plugins-compat-tester-cli/${pct_version}/plugins-compat-tester-cli-${pct_version}.jar"
 [ -f "${pct}" ] || mvn dependency:get -Dartifact=org.jenkins-ci.tests:plugins-compat-tester-cli:${pct_version}:jar -DremoteRepositories=repo.jenkins-ci.org::default::https://repo.jenkins-ci.org/public/,incrementals::default::https://repo.jenkins-ci.org/incrementals/ -Dtransitive=false
 cp "${pct}" target/pct.jar
