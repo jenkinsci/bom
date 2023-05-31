f11dec79b11a716627e898b6dd5c26afac87e1a7
Mark Waite <mark.earl.waite@gmail.com>
2023-05-31 10:14:49 -0600

    Use PCT 1336.v9d7ff9c73839
    Co-authored-by: Basil Crow <me@basilcrow.com>

--- prep-pct.sh
+++ prep-pct.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 cd "$(dirname "${0}")"
 
 # Tracked by ./updatecli/updatecli.d/plugin-compat-tester.yml
-pct_version=1332.v343b_3e353136
+pct_version=1336.v9d7ff9c73839
 pct="$(mvn -Dexpression=settings.localRepository -q -DforceStdout help:evaluate)/org/jenkins-ci/tests/plugins-compat-tester-cli/${pct_version}/plugins-compat-tester-cli-${pct_version}.jar"
 [ -f "${pct}" ] || mvn dependency:get -Dartifact=org.jenkins-ci.tests:plugins-compat-tester-cli:${pct_version}:jar -DremoteRepositories=repo.jenkins-ci.org::default::https://repo.jenkins-ci.org/public/,incrementals::default::https://repo.jenkins-ci.org/incrementals/ -Dtransitive=false
 cp "${pct}" target/pct.jar
