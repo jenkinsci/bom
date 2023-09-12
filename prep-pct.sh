659b00f9e813f496b7d68ebf6c95057f3e0edcf3
Basil Crow <me@basilcrow.com>
2023-09-12 13:07:58 -0700

    Remove more Updatecli references

--- prep-pct.sh
+++ prep-pct.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 cd "$(dirname "${0}")"
 
-# Tracked by ./updatecli/updatecli.d/plugin-compat-tester.yml
+# Tracked by .github/renovate.json
 pct_version=1337.vd1853588a_0c6
 pct="$(mvn -Dexpression=settings.localRepository -q -DforceStdout help:evaluate)/org/jenkins-ci/tests/plugins-compat-tester-cli/${pct_version}/plugins-compat-tester-cli-${pct_version}.jar"
 [ -f "${pct}" ] || mvn dependency:get -Dartifact=org.jenkins-ci.tests:plugins-compat-tester-cli:${pct_version}:jar -DremoteRepositories=repo.jenkins-ci.org::default::https://repo.jenkins-ci.org/public/,incrementals::default::https://repo.jenkins-ci.org/incrementals/ -Dtransitive=false
