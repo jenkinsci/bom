6142b25037d2e1df1700674f8838ec91e2f4e595
Adrien Lecharpentier <alecharpentier@cloudbees.com>
2024-02-29 09:53:28 +0100

    Do not append options to variable
    Thanks @basil.
    
    Co-Authored-By: Basil Crow <me@basilcrow.com>

--- local-test.sh
+++ local-test.sh
@@ -9,10 +9,8 @@ cd "$(dirname "$0")"
 LATEST_LINE=weekly
 : "${LINE:=$LATEST_LINE}"
 
-if [[ -n "${SAMPLE_PLUGIN_OPTS:-}" ]]; then
-	SAMPLE_PLUGIN_OPTS+=' -Dtest=InjectedTest'
-else
-	SAMPLE_PLUGIN_OPTS=-Dtest=InjectedTest
+if [[ -z "${SAMPLE_PLUGIN_OPTS:-}" ]]; then
+	SAMPLE_PLUGIN_OPTS="-Dtest=InjectedTest"
 fi
 if [[ $LINE != "${LATEST_LINE}" ]]; then
 	SAMPLE_PLUGIN_OPTS+=" -P${LINE}"
