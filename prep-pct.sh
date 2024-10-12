58f7b44b3a4496020a3d1f81511f106d4afffbd2
Basil Crow <me@basilcrow.com>
2024-10-12 16:23:19 -0700

    Test jenkinsci/maven-hpi-plugin#673

--- prep-pct.sh
+++ prep-pct.sh
@@ -3,7 +3,8 @@ set -euxo pipefail
 cd "$(dirname "${0}")"
 
 # Tracked by .github/renovate.json
-pct_version=1499.vd1b_0a_49cc79b_
+# TODO https://github.com/jenkinsci/plugin-compat-tester/pull/715
+pct_version=1518.v5c74e9e39c44
 pct="$(mvn -Dexpression=settings.localRepository -q -DforceStdout help:evaluate)/org/jenkins-ci/tests/plugins-compat-tester-cli/${pct_version}/plugins-compat-tester-cli-${pct_version}.jar"
 [ -f "${pct}" ] || mvn dependency:get -Dartifact=org.jenkins-ci.tests:plugins-compat-tester-cli:${pct_version}:jar -DremoteRepositories=repo.jenkins-ci.org::default::https://repo.jenkins-ci.org/public/,incrementals::default::https://repo.jenkins-ci.org/incrementals/ -Dtransitive=false
 cp "${pct}" target/pct.jar
