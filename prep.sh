d2d97e68c8647d0be9f08b9c8ae7528afddc58cd
Darin Pope <darin@planetpope.com>
2025-06-09 09:29:59 -0500

    change to incremental since the PR build is failing

--- prep.sh
+++ prep.sh
@@ -38,7 +38,7 @@ for LINE in $LINEZ; do
 			${PROFILE:-} \
 			-DoverrideWar="../target/megawar-${LINE}.war" \
 			-DuseUpperBounds \
-			-Dhpi-plugin.version=3.65 \
+			-Dhpi-plugin.version=3.66-rc1724.62ed6b_cf44b_b_ \
 			-DcommitHashes=target/commit-hashes.txt
 		mv sample-plugin/target/commit-hashes.txt "target/commit-hashes-${LINE}.txt"
 	fi
