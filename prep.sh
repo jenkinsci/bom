16e6db8d6b85152a196d8097dadc0460d7a4572f
Darin Pope <darin@planetpope.com>
2025-06-09 08:20:24 -0500

    Update to latest release version
    The incremental `3.62-rc1681.2ef7cf8c1fe3` was garbage collected in the past few days since it was in the `incrementals` repository and was older than 90 days.
    
    Updating to the latest release version (`3.65`).

--- prep.sh
+++ prep.sh
@@ -38,7 +38,7 @@ for LINE in $LINEZ; do
 			${PROFILE:-} \
 			-DoverrideWar="../target/megawar-${LINE}.war" \
 			-DuseUpperBounds \
-			-Dhpi-plugin.version=3.62-rc1681.2ef7cf8c1fe3 \
+			-Dhpi-plugin.version=3.65 \
 			-DcommitHashes=target/commit-hashes.txt
 		mv sample-plugin/target/commit-hashes.txt "target/commit-hashes-${LINE}.txt"
 	fi
