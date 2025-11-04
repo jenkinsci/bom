b3ecb308a74882b94355108aa43ec9c442ebc18b
Siddd-hrr <siddheshakole@gmail.com>
2025-11-04 22:19:17 +0530

    Removing override of hpi-plugin.version for launchable

--- prep.sh
+++ prep.sh
@@ -36,9 +36,7 @@ for LINE in $LINEZ; do
 			hpi:resolve-test-dependencies \
 			${SAMPLE_PLUGIN_OPTS:-} \
 			${PROFILE:-} \
-			-DoverrideWar="../target/megawar-${LINE}.war" \
 			-DuseUpperBounds \
-			-Dhpi-plugin.version=3.1749.v2ce5383a_7f9a_ \
 			-DcommitHashes=target/commit-hashes.txt
 		mv sample-plugin/target/commit-hashes.txt "target/commit-hashes-${LINE}.txt"
 	fi
