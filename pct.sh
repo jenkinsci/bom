9822f436039f57f44e399117818a4016073728c7
Mark Waite <mark.earl.waite@gmail.com>
2025-12-27 17:11:19 -0700

    Fall back to older jenkins test harness

--- pct.sh
+++ pct.sh
@@ -21,7 +21,7 @@ if ! [[ $PLUGINS =~ blueocean || $PLUGINS =~ lockable-resources || $PLUGINS =~ p
 fi
 
 # Tracked by .github/renovate.json
-JTH_VERSION=2554.v7da_7c803d140
+JTH_VERSION=2533.vb_b_4e14045e01
 if [[ $LINE == weekly ]]; then
 	PCT_D_ARGS+="-Djenkins-test-harness.version=${JTH_VERSION} "
 fi
