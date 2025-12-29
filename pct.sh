ee3bfad7ce6c0b3a2e10f4f7e2013f87f7833a7e
Tim Jacomb <timjacomb1@gmail.com>
2025-12-29 21:49:40 +0000

    Remove unneeded changes

--- pct.sh
+++ pct.sh
@@ -20,14 +20,6 @@ if ! [[ $PLUGINS =~ blueocean || $PLUGINS =~ lockable-resources || $PLUGINS =~ p
 	PCT_D_ARGS+='-DforkCount=.75C '
 fi
 
-# Tracked by .github/renovate.json
-JTH_VERSION=2541.v6e1dd03c77b_b_
-HPI_VERSION=3.1787.vd08d76c755ef
-if [[ $LINE == weekly ]]; then
-	PCT_D_ARGS+="-Djenkins-test-harness.version=${JTH_VERSION} "
-	PCT_D_ARGS+="-Dhpi-plugin.version=${HPI_VERSION} "
-fi
-
 exec java \
 	-Dorg.jenkins.tools.test.hook.JenkinsTestHarnessHook2.enabled \
 	-jar target/pct.jar \
