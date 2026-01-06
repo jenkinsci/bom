7095bcedc5c1fe85b5883c47b971926abd292cea
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2026-01-06 11:59:44 +0100

    Switch dependabot configs to renovate

--- prep-pct.sh
+++ prep-pct.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 cd "$(dirname "${0}")"
 
-# Tracked by .github/renovate.json
+# Tracked by .github/renovate.json5
 pct_version=1614.v891225c29865
 pct="$(mvn -Dexpression=settings.localRepository -q -DforceStdout help:evaluate)/org/jenkins-ci/tests/plugins-compat-tester-cli/${pct_version}/plugins-compat-tester-cli-${pct_version}.jar"
 [ -f "${pct}" ] || mvn dependency:get -Dartifact=org.jenkins-ci.tests:plugins-compat-tester-cli:${pct_version}:jar -DremoteRepositories=repo.jenkins-ci.org::default::https://repo.jenkins-ci.org/public/,incrementals::default::https://repo.jenkins-ci.org/incrementals/ -Dtransitive=false
