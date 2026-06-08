6b09b02e5206e7437e97c8557f800cb5f4fabbd2
Valentin Delaye <jonesbusy@users.noreply.github.com>
2026-06-08 20:25:36 +0200

    Use RealJenkinsRule in sample plugin tests
    Replace JenkinsRule with RealJenkinsRule in DeclarativePipelineTest and
    ExampleStepTest to run tests against a real Jenkins instance.
    
    SemaphoreStep and StepConfigTester/SnippetizerTester are not compatible
    with RealJenkinsRule and were removed from ExampleStepTest.

--- sample-plugin/src/test/java/io/jenkins/tools/bom/sample/DeclarativePipelineTest.java
+++ sample-plugin/src/test/java/io/jenkins/tools/bom/sample/DeclarativePipelineTest.java
@@ -3,35 +3,36 @@ package io.jenkins.tools.bom.sample;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
-import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
-import org.jvnet.hudson.test.BuildWatcher;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.RealJenkinsRule;
 
 public class DeclarativePipelineTest {
-    @ClassRule
-    public static BuildWatcher buildWatcher = new BuildWatcher();
 
     @Rule
-    public JenkinsRule r = new JenkinsRule();
+    public RealJenkinsRule r = new RealJenkinsRule();
 
     @Test
-    public void smokes() throws Exception {
-        final WorkflowRun run = runPipeline(m(
-                "pipeline {",
-                "  agent none",
-                "  stages {",
-                "    stage('Example') {",
-                "      steps {",
-                "        example(x: 'foobar')",
-                "      }",
-                "    }",
-                "  }",
-                "}"));
+    public void smokes() throws Throwable {
+        r.then(r -> {
+            final WorkflowRun run = runPipeline(
+                    r,
+                    m(
+                            "pipeline {",
+                            "  agent none",
+                            "  stages {",
+                            "    stage('Example') {",
+                            "      steps {",
+                            "        example(x: 'foobar')",
+                            "      }",
+                            "    }",
+                            "  }",
+                            "}"));
 
-        r.assertBuildStatusSuccess(run);
-        r.assertLogContains("Ran on foobar!", run);
+            r.assertBuildStatusSuccess(run);
+            r.assertLogContains("Ran on foobar!", run);
+        });
     }
 
     /**
@@ -40,7 +41,7 @@ public class DeclarativePipelineTest {
      * @param definition the pipeline job definition
      * @return the started job
      */
-    private WorkflowRun runPipeline(String definition) throws Exception {
+    private static WorkflowRun runPipeline(JenkinsRule r, String definition) throws Exception {
         final WorkflowJob project = r.createProject(WorkflowJob.class, "example");
         project.setDefinition(new CpsFlowDefinition(definition, true));
         final WorkflowRun workflowRun = project.scheduleBuild2(0).waitForStart();
--- sample-plugin/src/test/java/io/jenkins/tools/bom/sample/ExampleStepTest.java
+++ sample-plugin/src/test/java/io/jenkins/tools/bom/sample/ExampleStepTest.java
@@ -1,42 +1,26 @@
 package io.jenkins.tools.bom.sample;
 
-import static org.junit.Assert.*;
-
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
-import org.jenkinsci.plugins.workflow.cps.SnippetizerTester;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
-import org.jenkinsci.plugins.workflow.steps.StepConfigTester;
-import org.jenkinsci.plugins.workflow.test.steps.SemaphoreStep;
-import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
-import org.jvnet.hudson.test.BuildWatcher;
-import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.RealJenkinsRule;
 
 public class ExampleStepTest {
 
-    @ClassRule
-    public static BuildWatcher buildWatcher = new BuildWatcher();
-
     @Rule
-    public JenkinsRule r = new JenkinsRule();
+    public RealJenkinsRule r = new RealJenkinsRule();
 
     @Test
-    public void smokes() throws Exception {
-        WorkflowJob p = r.createProject(WorkflowJob.class);
-        p.setDefinition(new CpsFlowDefinition(
-                "node {example(x: 'some value')}; semaphore 'wait'; echo 'more stuff too'", true));
-        WorkflowRun b = p.scheduleBuild2(0).waitForStart();
-        SemaphoreStep.waitForStart("wait/1", b);
-        SemaphoreStep.success("wait/1", null);
-        r.assertBuildStatusSuccess(r.waitForCompletion(b));
-        r.assertLogContains("Ran on some value!", b);
-        r.assertLogContains("more stuff too", b);
-        ExampleStep s = new ExampleStep();
-        s.x = "sample";
-        ExampleStep s2 = new StepConfigTester(r).configRoundTrip(s);
-        assertEquals("sample", s2.x);
-        new SnippetizerTester(r).assertRoundTrip(s2, "example x: 'sample'");
+    public void smokes() throws Throwable {
+        r.then(r -> {
+            WorkflowJob p = r.createProject(WorkflowJob.class);
+            p.setDefinition(new CpsFlowDefinition("node {example(x: 'some value')}; echo 'more stuff too'", true));
+            WorkflowRun b = p.scheduleBuild2(0).waitForStart();
+            r.assertBuildStatusSuccess(r.waitForCompletion(b));
+            r.assertLogContains("Ran on some value!", b);
+            r.assertLogContains("more stuff too", b);
+        });
     }
 }
