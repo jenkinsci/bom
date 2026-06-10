6307c2d13350fb516ac0e809315f308015d7fa68
Valentin Delaye <jonesbusy@users.noreply.github.com>
2026-06-10 14:59:41 +0200

    Adress PR comment, restore assertions and keep roundTripTest using JenkinsRule

--- sample-plugin/src/test/java/io/jenkins/tools/bom/sample/DeclarativePipelineTest.java
+++ sample-plugin/src/test/java/io/jenkins/tools/bom/sample/DeclarativePipelineTest.java
@@ -16,19 +16,18 @@ public class DeclarativePipelineTest {
     @Test
     public void smokes() throws Throwable {
         r.then(r -> {
-            final WorkflowRun run = runPipeline(
-                    r,
-                    m(
-                            "pipeline {",
-                            "  agent none",
-                            "  stages {",
-                            "    stage('Example') {",
-                            "      steps {",
-                            "        example(x: 'foobar')",
-                            "      }",
-                            "    }",
-                            "  }",
-                            "}"));
+            final WorkflowRun run = runPipeline(r, """
+                    pipeline {
+                      agent none
+                      stages {
+                        stage('Example') {
+                          steps {
+                            example(x: 'foobar')
+                          }
+                        }
+                      }
+                    }
+                    """);
 
             r.assertBuildStatusSuccess(run);
             r.assertLogContains("Ran on foobar!", run);
@@ -47,15 +46,5 @@ public class DeclarativePipelineTest {
         final WorkflowRun workflowRun = project.scheduleBuild2(0).waitForStart();
         r.waitForCompletion(workflowRun);
         return workflowRun;
-    }
-
-    /**
-     * Approximates a multiline string in Java.
-     *
-     * @param lines the lines to concatenate with a newline separator
-     * @return the concatenated multiline string
-     */
-    private static String m(String... lines) {
-        return String.join("\n", lines);
     }
 }
--- sample-plugin/src/test/java/io/jenkins/tools/bom/sample/ExampleStepTest.java
+++ sample-plugin/src/test/java/io/jenkins/tools/bom/sample/ExampleStepTest.java
@@ -1,10 +1,16 @@
 package io.jenkins.tools.bom.sample;
 
+import static org.junit.Assert.assertEquals;
+
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.cps.SnippetizerTester;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.jenkinsci.plugins.workflow.steps.StepConfigTester;
+import org.jenkinsci.plugins.workflow.support.steps.input.InputAction;
 import org.junit.Rule;
 import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.RealJenkinsRule;
 
 public class ExampleStepTest {
@@ -12,15 +18,34 @@ public class ExampleStepTest {
     @Rule
     public RealJenkinsRule r = new RealJenkinsRule();
 
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
+
     @Test
     public void smokes() throws Throwable {
         r.then(r -> {
             WorkflowJob p = r.createProject(WorkflowJob.class);
-            p.setDefinition(new CpsFlowDefinition("node {example(x: 'some value')}; echo 'more stuff too'", true));
+            p.setDefinition(new CpsFlowDefinition(
+                    "node {example(x: 'some value')}; input 'wait'; echo 'more stuff too'", true));
             WorkflowRun b = p.scheduleBuild2(0).waitForStart();
+            while (true) {
+                InputAction ia = b.getAction(InputAction.class);
+                if (ia != null && ia.isWaitingForInput()) break;
+                Thread.sleep(100);
+            }
+            b.getAction(InputAction.class).getExecutions().get(0).doProceedEmpty();
             r.assertBuildStatusSuccess(r.waitForCompletion(b));
             r.assertLogContains("Ran on some value!", b);
             r.assertLogContains("more stuff too", b);
         });
+    }
+
+    @Test
+    public void configRoundTrip() throws Exception {
+        ExampleStep s = new ExampleStep();
+        s.x = "sample";
+        ExampleStep s2 = new StepConfigTester(j).configRoundTrip(s);
+        assertEquals("sample", s2.x);
+        new SnippetizerTester(j).assertRoundTrip(s2, "example x: 'sample'");
     }
 }
