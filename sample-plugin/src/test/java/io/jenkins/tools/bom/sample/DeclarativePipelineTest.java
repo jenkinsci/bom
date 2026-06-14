b6b23f9003a2c23d6f44ad90c3ed8247f4b70b58
Valentin Delaye <jonesbusy@users.noreply.github.com>
2026-06-14 22:57:50 +0200

    Use `RealJenkinsRule` when possible in sample plugin tests (#6935)

--- sample-plugin/src/test/java/io/jenkins/tools/bom/sample/DeclarativePipelineTest.java
+++ sample-plugin/src/test/java/io/jenkins/tools/bom/sample/DeclarativePipelineTest.java
@@ -3,35 +3,35 @@ package io.jenkins.tools.bom.sample;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
-import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
-import org.jvnet.hudson.test.BuildWatcher;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.RealJenkinsRule;
 
 public class DeclarativePipelineTest {
-    @ClassRule
-    public static BuildWatcher buildWatcher = new BuildWatcher();
 
     @Rule
-    public JenkinsRule r = new JenkinsRule();
+    public RealJenkinsRule r = new RealJenkinsRule();
 
     @Test
-    public void smokes() throws Exception {
-        final WorkflowRun run = runPipeline(m(
-                "pipeline {",
-                "  agent none",
-                "  stages {",
-                "    stage('Example') {",
-                "      steps {",
-                "        example(x: 'foobar')",
-                "      }",
-                "    }",
-                "  }",
-                "}"));
+    public void smokes() throws Throwable {
+        r.then(r -> {
+            final WorkflowRun run = runPipeline(r, """
+                    pipeline {
+                      agent none
+                      stages {
+                        stage('Example') {
+                          steps {
+                            example(x: 'foobar')
+                          }
+                        }
+                      }
+                    }
+                    """);
 
-        r.assertBuildStatusSuccess(run);
-        r.assertLogContains("Ran on foobar!", run);
+            r.assertBuildStatusSuccess(run);
+            r.assertLogContains("Ran on foobar!", run);
+        });
     }
 
     /**
@@ -40,21 +40,11 @@ public class DeclarativePipelineTest {
      * @param definition the pipeline job definition
      * @return the started job
      */
-    private WorkflowRun runPipeline(String definition) throws Exception {
+    private static WorkflowRun runPipeline(JenkinsRule r, String definition) throws Exception {
         final WorkflowJob project = r.createProject(WorkflowJob.class, "example");
         project.setDefinition(new CpsFlowDefinition(definition, true));
         final WorkflowRun workflowRun = project.scheduleBuild2(0).waitForStart();
         r.waitForCompletion(workflowRun);
         return workflowRun;
-    }
-
-    /**
-     * Approximates a multiline string in Java.
-     *
-     * @param lines the lines to concatenate with a newline separator
-     * @return the concatenated multiline string
-     */
-    private static String m(String... lines) {
-        return String.join("\n", lines);
     }
 }
